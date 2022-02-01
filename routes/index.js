dd4f9ed745fae8fe2e06e195a5ca2b42e7d9e550
brt9029 <brt9029@gmail.com>
2022-02-01 17:34:04 -0600

    completed routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
+// Import all of the API routes from /api/index.js ( no need for index.js since it is implied)
+const apiRoutes = require('./api')
 const htmlRoutes = require('./html/html-routes');
 
+// add '/api' prefix to all api routes imported from api directory
+router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
 
 router.use((req, res) => {
